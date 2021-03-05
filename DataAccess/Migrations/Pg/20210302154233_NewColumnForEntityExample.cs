a0a39f7607761354d8b80f426d60ec0323af3172
furkanaltaca <furkanaltaca@gmail.com>
2021-03-05 13:17:06 +0300

    Create migrations for postgres

---
+++ DataAccess/Migrations/Pg/20210302154233_NewColumnForEntityExample.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations.Pg
+{
+    public partial class NewColumnForEntityExample : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "EntityExamples",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "EntityExamples");
+        }
+    }
+}
